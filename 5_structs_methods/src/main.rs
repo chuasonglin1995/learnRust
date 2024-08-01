98ee0bf4c2bf1150d3a441e7a697fee9d68922f5
chuasonglin1995 <chuasonglin1995@gmail.com>
2024-08-01 07:00:30 +0100

    finish lesson 5

--- 5_structs_methods/src/main.rs
+++ 5_structs_methods/src/main.rs
@@ -5,9 +5,28 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
+    fn area(&self) -> u32 { // &self is actually short for self: &Self
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+}
+
+impl Rectangle {
+    // `new` isnt a special name and isnt built into the language
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+    
 }
 
 fn main() {
@@ -20,4 +39,22 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    if rect1.width() { // is actually (&rect1).width(), rust does automatic referencing
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(3);
 }
